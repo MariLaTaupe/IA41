0fbd866f04feb65c0c891c0167747c613901666e
MariLaTaupe <pdtv1997@gmail.com>
2017-05-09 11:45:36 +0200

    Codages configuration avant réponse du prof

---
+++ decision.pl
@@ -0,0 +1,17 @@
+% GameConfiguration :
+% [TL,TR,BL,BR,TargetId,BlueRobotPosition,GreenRobotPosition,YellowRobotPosition,RedRobotPosition)
+% TL : (bool)
+% TR : (bool)
+% BL : (bool)
+% BR : (bool)
+% TargetId : (int) de 0 à 16, numéro de la cible
+% BlueRobotPosition : [x,y], position du robot bleu
+% GreenRobotPosition : [x,y], position du robot vert
+% YellowRobotPosition : [x,y], position du robot  jaune
+% RedRobotPosition : [x,y], position du robot rouge
+% x,y : (int*int) coordonées
+% 
+% 
+% 
+% 
+% 
