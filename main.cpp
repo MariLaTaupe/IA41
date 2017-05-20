c600008a213a6b6393273f0adb59c20ab93407a4
MariLaTaupe <pdtv1997@gmail.com>
2017-05-20 18:14:07 +0200

    Ajout de NouvellePartie au main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
+#include "nouvellepartie.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
+    NouvellePartie n;
+    n.show();
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "nouvellepartie.h"
+#include "ui_nouvellepartie.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
