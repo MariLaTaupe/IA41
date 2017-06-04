e9edddd9e716fadcbb910b385e82c9b00f8e941c
Prunelle <pdtv1997@gmail.com>
2017-06-04 12:10:32 +0200

    Ajout d'un pion au clic pour chaque case

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -32,7 +32,183 @@ int MainWindow::displayLabel2(){
         return 0;
 }
 
+int MainWindow::displayLabel3(){
+    int wid,hei;
+
+        wid = ui->label_3->geometry().width();
+        hei = ui->label_3->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_3->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel4(){
+    int wid,hei;
+
+        wid = ui->label_4->geometry().width();
+        hei = ui->label_4->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_4->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel5(){
+    int wid,hei;
+
+        wid = ui->label_5->geometry().width();
+        hei = ui->label_5->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_5->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel6(){
+    int wid,hei;
+
+        wid = ui->label_6->geometry().width();
+        hei = ui->label_6->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_6->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel7(){
+    int wid,hei;
+
+        wid = ui->label_7->geometry().width();
+        hei = ui->label_7->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_7->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel8(){
+    int wid,hei;
+
+        wid = ui->label_8->geometry().width();
+        hei = ui->label_8->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_8->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel9(){
+    int wid,hei;
+
+        wid = ui->label_9->geometry().width();
+        hei = ui->label_9->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_9->setPixmap(pixmap);
+
+        return 0;
+}
+
+int MainWindow::displayLabel10(){
+    int wid,hei;
+
+        wid = ui->label_10->geometry().width();
+        hei = ui->label_10->geometry().height();
+
+        QPixmap pixmap(wid,hei);
+        pixmap.fill(QColor("white"));
+        QPainter painter(&pixmap);
+
+        painter.setBrush(QBrush(Qt::green));
+        painter.drawEllipse( 60, 130, 80, 50);
+        ui->label_10->setPixmap(pixmap);
+
+        return 0;
+}
+
 void MainWindow::on_label_2_clicked()
 {
     displayLabel2();
+}
+
+void MainWindow::on_label_3_clicked()
+{
+    displayLabel3();
+}
+
+void MainWindow::on_label_4_clicked()
+{
+    displayLabel4();
+}
+
+void MainWindow::on_label_5_clicked()
+{
+    displayLabel5();
+}
+
+void MainWindow::on_label_6_clicked()
+{
+    displayLabel6();
+}
+
+void MainWindow::on_label_7_clicked()
+{
+    displayLabel7();
+}
+
+void MainWindow::on_label_8_clicked()
+{
+    displayLabel8();
+}
+
+void MainWindow::on_label_9_clicked()
+{
+    displayLabel9();
+}
+
+void MainWindow::on_label_10_clicked()
+{
+    displayLabel10();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -20,9 +20,25 @@ public:
 
 private slots:
     void on_label_2_clicked();
+    void on_label_3_clicked();
+    void on_label_4_clicked();
+    void on_label_5_clicked();
+    void on_label_6_clicked();
+    void on_label_7_clicked();
+    void on_label_8_clicked();
+    void on_label_9_clicked();
+    void on_label_10_clicked();
 
 public slots:
     int displayLabel2();
+    int displayLabel3();
+    int displayLabel4();
+    int displayLabel5();
+    int displayLabel6();
+    int displayLabel7();
+    int displayLabel8();
+    int displayLabel9();
+    int displayLabel10();
 
 private:
     Ui::MainWindow *ui;
