87127814ccb8675df78729f5410c7153ff4e38c9
Prunelle <pdtv1997@gmail.com>
2017-06-15 08:44:23 +0200

    Re-implementation de displayLabel : tous les labels d'un coup

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <time.h> //Ne pas oublier d'inclure le fichier time.h
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
@@ -16,77 +15,61 @@ MainWindow::MainWindow(QWidget *parent) :
 
     jeu = new Jeu();
 
-    nbPionsLabel2 = 1; // Case en haut à gauche
-    pionsLabel2[0] = -1;
-    pionsLabel2[1] = -1;
-    pionsLabel2[2] = -1;
-    pionsLabel2[3] = -1;
+    pionsLabel2[0] = 1; // Case en haut à gauche
+    pionsLabel2[1] = -1; // -1 : pas de pions
+    pionsLabel2[2] = -1; // 1 : WHITE
+    pionsLabel2[3] = -1; // 2 : BLACK
     pionsLabel2[4] = -1;
-    displayLabel2(true,0);
 
-    nbPionsLabel3 = 1; // Case en haut à droite
-    pionsLabel3[0] = -1;
+    pionsLabel3[0] = 1; // Case en haut à droite
     pionsLabel3[1] = -1;
     pionsLabel3[2] = -1;
     pionsLabel3[3] = -1;
     pionsLabel3[4] = -1;
-    displayLabel3();
 
-    nbPionsLabel4 = 1; // Case en haut au millieu
-    pionsLabel4[0] = -1;
+    pionsLabel4[0] = 1; // Case en haut au millieu
     pionsLabel4[1] = -1;
     pionsLabel4[2] = -1;
     pionsLabel4[3] = -1;
     pionsLabel4[4] = -1;
-    displayLabel4();
 
-    nbPionsLabel5 = 1; // Case en bas à droite
-    pionsLabel5[0] = -1;
+    pionsLabel5[0] = 2; // Case en bas à droite
     pionsLabel5[1] = -1;
     pionsLabel5[2] = -1;
     pionsLabel5[3] = -1;
     pionsLabel5[4] = -1;
-    displayLabel5();
 
-    nbPionsLabel6 = 1; // Case en bas au millieu
-    pionsLabel6[0] = -1;
+    pionsLabel6[0] = 2; // Case en bas au millieu
     pionsLabel6[1] = -1;
     pionsLabel6[2] = -1;
     pionsLabel6[3] = -1;
     pionsLabel6[4] = -1;
-    displayLabel6();
 
-    nbPionsLabel7 = 1; // Case en bas à gauche
-    pionsLabel7[0] = -1;
+    pionsLabel7[0] = 2; // Case en bas à gauche
     pionsLabel7[1] = -1;
     pionsLabel7[2] = -1;
     pionsLabel7[3] = -1;
     pionsLabel7[4] = -1;
-    displayLabel7();
 
-    nbPionsLabel8 = 1; // Case au millieu à gauche
-    pionsLabel8[0] = -1;
+    pionsLabel8[0] = -1; // Case au millieu à gauche
     pionsLabel8[1] = -1;
     pionsLabel8[2] = -1;
     pionsLabel8[3] = -1;
     pionsLabel8[4] = -1;
-    displayLabel8();
 
-    nbPionsLabel9 = 1; // Case centrale
-    pionsLabel9[0] = -1;
+    pionsLabel9[0] = -1; // Case centrale
     pionsLabel9[1] = -1;
     pionsLabel9[2] = -1;
     pionsLabel9[3] = -1;
     pionsLabel9[4] = -1;
-    displayLabel9();
 
-    nbPionsLabel10 = 1; // Case au millieu à droite
-    pionsLabel10[0] = -1;
+    pionsLabel10[0] = -1; // Case au millieu à droite
     pionsLabel10[1] = -1;
     pionsLabel10[2] = -1;
     pionsLabel10[3] = -1;
     pionsLabel10[4] = -1;
-    displayLabel10();
+
+    displayLabel();
 
     joueur1 = 1; // 1 = humain, 2,3,4 ... = ia
     joueur2 = 2;
@@ -110,370 +93,146 @@ MainWindow::~MainWindow()
 }
 
 // TODO : display en fonction du modèle
-/*
+
 void MainWindow::displayLabel(){
-
-}
-*/
-/*
-int MainWindow::displayLabel2(bool sens, int nbPions){ // Case en haut à gauche
-
-    if(sens){
-        nbPionsLabel2 = nbPionsLabel2 + nbPions;
-    }else{
-        nbPionsLabel2 = nbPionsLabel2 - nbPions;
-    }
 
     int wid,hei;
     wid = ui->label_2->geometry().width();
     hei = ui->label_2->geometry().height();
 
-    srand(time(NULL)); // initialisation de rand
+    QPixmap pixmap2(wid,hei);
+    pixmap2.fill(QColor("white"));
+    QPainter painter2(&pixmap2);
 
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
+    QPixmap pixmap3(wid,hei);
+    pixmap3.fill(QColor("white"));
+    QPainter painter3(&pixmap3);
+
+    QPixmap pixmap4(wid,hei);
+    pixmap4.fill(QColor("white"));
+    QPainter painter4(&pixmap4);
+
+    QPixmap pixmap5(wid,hei);
+    pixmap5.fill(QColor("white"));
+    QPainter painter5(&pixmap5);
+
+    QPixmap pixmap6(wid,hei);
+    pixmap6.fill(QColor("white"));
+    QPainter painter6(&pixmap6);
+
+    QPixmap pixmap7(wid,hei);
+    pixmap7.fill(QColor("white"));
+    QPainter painter7(&pixmap7);
+
+    QPixmap pixmap8(wid,hei);
+    pixmap8.fill(QColor("white"));
+    QPainter painter8(&pixmap8);
+
+    QPixmap pixmap9(wid,hei);
+    pixmap9.fill(QColor("white"));
+    QPainter painter9(&pixmap9);
+
+    QPixmap pixmap10(wid,hei);
+    pixmap10.fill(QColor("white"));
+    QPainter painter10(&pixmap10);
+
     int i=0;
-    QPainter painter(&pixmap);
 
-    if(nbPionsLabel2<6 && nbPionsLabel2>0){
-        for(i=0;i<nbPionsLabel2;++i){
-            if (pionsLabel2[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel2[i] = 0;
-                }else{
-                    pionsLabel2[i] = 1;
-                }
-            }
-
+    for(i=0;i<4;++i){
+        if(pionsLabel2[i] != -1){
             if(pionsLabel2[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter2.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel2[i] == 2){
+                painter2.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter2.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_2->setPixmap(pixmap);
-        nbPionsLabel2++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel3(){ // Case en haut à droite
-    int wid,hei;
-    wid = ui->label_3->geometry().width();
-    hei = ui->label_3->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel3<6 && nbPionsLabel3>0){
-        for(i=0;i<nbPionsLabel3;++i){
-            if (pionsLabel3[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel3[i] = 0;
-                }else{
-                    pionsLabel3[i] = 1;
-                }
-            }
-
+        if(pionsLabel3[i] != -1){
             if(pionsLabel3[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter3.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel3[i] == 2){
+                painter3.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter3.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_3->setPixmap(pixmap);
-        nbPionsLabel3++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel4(){ // Case en haut au millieu
-    int wid,hei;
-    wid = ui->label_4->geometry().width();
-    hei = ui->label_4->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel4<6 && nbPionsLabel4>0){
-        for(i=0;i<nbPionsLabel4;++i){
-            if (pionsLabel4[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel4[i] = 0;
-                }else{
-                    pionsLabel4[i] = 1;
-                }
-            }
-
+        if(pionsLabel4[i] != -1){
             if(pionsLabel4[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter4.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel4[i] == 2){
+                painter4.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter4.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_4->setPixmap(pixmap);
-        nbPionsLabel4++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel5(){ // Case en bas à droite
-    int wid,hei;
-    wid = ui->label_5->geometry().width();
-    hei = ui->label_5->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel5<6 && nbPionsLabel5>0){
-        for(i=0;i<nbPionsLabel5;++i){
-            if (pionsLabel5[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel5[i] = 0;
-                }else{
-                    pionsLabel5[i] = 1;
-                }
-            }
-
+        if(pionsLabel5[i] != -1){
             if(pionsLabel5[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter5.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel5[i] == 2){
+                painter5.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter5.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_5->setPixmap(pixmap);
-        nbPionsLabel5++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel6(){ // Case en bas au millieu
-    int wid,hei;
-    wid = ui->label_6->geometry().width();
-    hei = ui->label_6->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel6<6 && nbPionsLabel6>0){
-        for(i=0;i<nbPionsLabel6;++i){
-            if (pionsLabel6[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel6[i] = 0;
-                }else{
-                    pionsLabel6[i] = 1;
-                }
-            }
-
+        if(pionsLabel6[i] != -1){
             if(pionsLabel6[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter6.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel6[i] == 2){
+                painter6.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter6.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_6->setPixmap(pixmap);
-        nbPionsLabel6++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel7(){ // Case en bas à gauche
-    int wid,hei;
-    wid = ui->label_7->geometry().width();
-    hei = ui->label_7->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel7<6 && nbPionsLabel7>0){
-        for(i=0;i<nbPionsLabel7;++i){
-            if (pionsLabel7[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel7[i] = 0;
-                }else{
-                    pionsLabel7[i] = 1;
-                }
-            }
-
+        if(pionsLabel7[i] != -1){
             if(pionsLabel7[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter7.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel7[i] == 2){
+                painter7.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter7.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_7->setPixmap(pixmap);
-        nbPionsLabel7++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel8(){ // Case au millieu à gauche
-    int wid,hei;
-    wid = ui->label_8->geometry().width();
-    hei = ui->label_8->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel8<6 && nbPionsLabel8>0){
-        for(i=0;i<nbPionsLabel8;++i){
-            if (pionsLabel8[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel8[i] = 0;
-                }else{
-                    pionsLabel8[i] = 1;
-                }
-            }
-
+        if(pionsLabel8[i] != -1){
             if(pionsLabel8[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter8.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel8[i] == 2){
+                painter8.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter8.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_8->setPixmap(pixmap);
-        nbPionsLabel8++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel9(){ // Case centrale
-    int wid,hei;
-    wid = ui->label_9->geometry().width();
-    hei = ui->label_9->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel9<6 && nbPionsLabel9>0){
-        for(i=0;i<nbPionsLabel9;++i){
-            if (pionsLabel9[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel9[i] = 0;
-                }else{
-                    pionsLabel9[i] = 1;
-                }
-            }
-
+        if(pionsLabel9[i] != -1){
             if(pionsLabel9[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter9.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel9[i] == 2){
+                painter9.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter9.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_9->setPixmap(pixmap);
-        nbPionsLabel9++;
-    }
-
-    return 0;
-}
-
-int MainWindow::displayLabel10(){ // Case au millieu à droite
-    int wid,hei;
-    wid = ui->label_10->geometry().width();
-    hei = ui->label_10->geometry().height();
-
-    srand(time(NULL)); // initialisation de rand
-
-    QPixmap pixmap(wid,hei);
-    pixmap.fill(QColor("white"));
-    int i=0;
-    QPainter painter(&pixmap);
-
-    if(nbPionsLabel10<6 && nbPionsLabel10>0){
-        for(i=0;i<nbPionsLabel10;++i){
-            if (pionsLabel10[i] == -1){
-                int al = rand()%201;
-                if(al < 101){
-                    pionsLabel10[i] = 0;
-                }else{
-                    pionsLabel10[i] = 1;
-                }
-            }
-
+        if(pionsLabel10[i] != -1){
             if(pionsLabel10[i] == 1){
-                painter.setBrush(QBrush(Qt::red));
-            }else{
-                painter.setBrush(QBrush(Qt::blue));
+                painter10.setBrush(QBrush(Qt::red));
+            }else if(pionsLabel10[i] == 2){
+                painter10.setBrush(QBrush(Qt::blue));
             }
-
-            painter.drawEllipse( 60, 130 - 25*i, 80, 50);
+            painter10.drawEllipse( 60, 130 - 25*i, 80, 50);
         }
 
-        ui->label_10->setPixmap(pixmap);
-        nbPionsLabel10++;
     }
 
-    return 0;
+    ui->label_2->setPixmap(pixmap2);
+    ui->label_3->setPixmap(pixmap3);
+    ui->label_4->setPixmap(pixmap4);
+    ui->label_5->setPixmap(pixmap5);
+    ui->label_6->setPixmap(pixmap6);
+    ui->label_7->setPixmap(pixmap7);
+    ui->label_8->setPixmap(pixmap8);
+    ui->label_9->setPixmap(pixmap9);
+    ui->label_10->setPixmap(pixmap10);
+
 }
-*/
 
 void MainWindow::on_pushButton_ok_clicked()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -26,18 +26,7 @@ private slots:
     void on_comboBox_pions_currentIndexChanged(const QString &arg1);
 
 public slots:
-    int displayLabel();
-    /*
-    int displayLabel2(bool sens, int nbPions);
-    int displayLabel3();
-    int displayLabel4();
-    int displayLabel5();
-    int displayLabel6();
-    int displayLabel7();
-    int displayLabel8();
-    int displayLabel9();
-    int displayLabel10();
-    */
+    void displayLabel();
     void letsPlay(const Position2D &firstStack, const Position2D &secondStack, int n, Color c);
 
 private:
@@ -57,34 +46,15 @@ private:
     QString c8;
     QString c9;
 
-    int nbPionsLabel2;
     int pionsLabel2[5];
-
-    int nbPionsLabel3;
     int pionsLabel3[5];
-
-    int nbPionsLabel4;
     int pionsLabel4[5];
-
-    int nbPionsLabel5;
     int pionsLabel5[5];
-
-    int nbPionsLabel6;
     int pionsLabel6[5];
-
-    int nbPionsLabel7;
     int pionsLabel7[5];
-
-    int nbPionsLabel8;
     int pionsLabel8[5];
-
-    int nbPionsLabel9;
     int pionsLabel9[5];
-
-    int nbPionsLabel10;
     int pionsLabel10[5];
 };
-
-void letsPlay(const Position2D& firstStack,  const Position2D& secondStack, int n, Color c);
 
 #endif // MAINWINDOW_H
